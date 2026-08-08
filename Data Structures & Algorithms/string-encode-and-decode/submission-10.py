5aa1da8e9cb68b3dcf84a90ddc1ca56bd96deda4
Ameen Durani <140562992+AmeenDurani@users.noreply.github.com>
2026-08-08 18:10:20 +0800

    Add: string-encode-and-decode - submission-10

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-10.py
@@ -0,0 +1,29 @@
+class Solution:
+    separator = ">"
+
+    def encode(self, strs: List[str]) -> str:
+        res = ""
+        for s in strs:
+            length = str(len(s))
+            res += length + self.separator + s
+        return res
+
+    def separate(self, sep_index: int, s_len: int, s: str) -> str:
+        if (sep_index + s_len) >= len(s): return ""
+        return s[sep_index + 1 : sep_index + s_len + 1]
+
+    def decode(self, s: str) -> List[str]:
+        strs = []
+        s_len = ""
+        i, s_max = 0, len(s)
+
+        while i < s_max:
+            if s[i] == self.separator:
+                i_len = int(s_len)
+                strs.append(self.separate(i, i_len, s))
+                i = i + i_len
+                s_len = ""
+            else:
+                s_len += s[i]
+            i += 1  
+        return strs
